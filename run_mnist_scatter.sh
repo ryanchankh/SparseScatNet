39b83223b2293eee1e24236a8008c23b703733eb
Ryan Chan <ryanchankh@berkeley.edu>
2021-11-06 21:06:25 -0400

    add mnist script

---
+++ run_mnist_scatter.sh
@@ -0,0 +1,42 @@
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 2 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 4 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 6 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 8 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 16 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 32 --scattering-wph
+
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 2 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 4 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 6 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 8 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 16 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 32 --scattering-wph
+
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 2 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 4 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 6 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 8 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 16 --scattering-wph
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 32 --scattering-wph
+
+
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 2 
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 4 
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 6 
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 8 
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 16 
+python3 main_redunet.py DIR --scattering-J 1 --scat-angles 32
+
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 2 
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 4 
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 6 
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 8 
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 16 
+python3 main_redunet.py DIR --scattering-J 2 --scat-angles 32
+
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 2  
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 4  
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 6  
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 8  
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 16  
+python3 main_redunet.py DIR --scattering-J 3 --scat-angles 32 
